cf49e1a68d85da411391aaf6fa72c3647477fb5b
kinokus <aleks.kinokus@gmail.com>
2021-01-28 10:18:38 +0200

    start

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,22 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <NavHeader></NavHeader>
     <router-view/>
   </div>
 </template>
+
+<script>
+
+import NavHeader from '@/components/NavHeader.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    NavHeader,
+  },
+};
+
+</script>
 
 <style lang="scss">
 #app {
@@ -17,16 +27,4 @@
   color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
---
+++ src/components/NavHeader.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-content class="nav-header">
+    <v-container d-flex>
+      <router-link to="/">Home</router-link>
+      |
+      <router-link to="/about" class="accent-4">About</router-link>
+    </v-container>
+  </v-content>
+</template>
+
+<script>
+export default {
+  name: 'NavHeader',
+  props: {},
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.nav-header {
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+
+}
+</style>
