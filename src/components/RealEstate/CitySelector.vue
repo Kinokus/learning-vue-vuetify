acc5d2e01df350df225ab909d1d61e149d840319
kinokus <aleks.kinokus@gmail.com>
2021-01-29 15:17:25 +0200

    new components

---
+++ src/components/RealEstate/CitySelector.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="city-selector">
+    <label>
+      <select v-model="selected">
+        <option disabled value="">Please select one</option>
+        <option
+            v-for="city in cities"
+            v-bind:key="city.cityId"
+            v-bind:value="city">
+          {{ city.cityName }}
+        </option>
+      </select>
+    </label>
+    <span> | </span>
+    <span>Selected: {{ selected.cityName }}</span>
+  </div>
+</template>
+
+<script>
+// import axios from 'axios';
+import { mapState } from 'vuex';
+
+export default {
+  name: 'CitySelector',
+  data() {
+    return {
+      selected: '',
+      // cities: [],
+      errors: [],
+    };
+  },
+  async created() {
+    await this.$store.dispatch('fetchCities');
+  },
+
+  // todo or this one
+  computed:
+      mapState(['cities']),
+
+  // todo or this one
+  // computed:
+  //     mapState({ cities: (state) => state.cities }),
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/RealEstate/HoodSelector.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="hood-selector"></div>
+</template>
+
+<script>
+export default {
+  name: 'HoodSelector',
+};
+</script>
+
+<style scoped>
+
+</style>
