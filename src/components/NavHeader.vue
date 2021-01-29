a9b2d00d8af2f4637106d863bd1bacb8a3e76013
kinokus <aleks.kinokus@gmail.com>
2021-01-29 10:57:30 +0200

    new route + reuse routes

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -1,17 +1,37 @@
 <template>
-  <v-content class="nav-header">
-    <v-container d-flex>
-      <router-link to="/">Home</router-link>
-      |
-      <router-link to="/about" class="accent-4">About</router-link>
+  <v-main class="nav-header">
+    <v-container class="pa-1">
+
+      <router-link
+          v-for="(route, index) in routes"
+          v-bind:to="route.path.length ? route.path : '/'"
+          v-bind:key="route.path"
+          v-bind:tooltip="index"
+      >
+        {{ route.name }}
+      </router-link>
+      <!--      <router-link to="/">Home</router-link>-->
+      <!--      |-->
+      <!--      <router-link to="/about" class="accent-4">About</router-link>-->
+      <!--      |-->
+      <!--      <router-link to="/realEstate" class="accent-4">Real Estate</router-link>-->
     </v-container>
-  </v-content>
+  </v-main>
 </template>
 
 <script>
+import router from '../router/index';
+
 export default {
+
   name: 'NavHeader',
-  props: {},
+  props: {
+    // routes: router.getRoutes(),
+  },
+  created() {
+    this.routes = router.getRoutes();
+    console.log(this.routes);
+  },
 };
 </script>
 
@@ -26,6 +46,13 @@ export default {
     &.router-link-exact-active {
       color: #42b983;
     }
+
+    &:not(:last-child){
+      &:after{
+        content: ' | ';
+      }
+    }
+
   }
 
 }
---
+++ src/views/RealEstate.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="real-estate">
+    RealEstate
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RealEstate',
+};
+</script>
+
+<style scoped>
+
+</style>
