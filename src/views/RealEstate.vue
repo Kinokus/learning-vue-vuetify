67aa918a898923bc0b185cab6b72b16e97d480fd
kinokus <aleks.kinokus@gmail.com>
2021-01-29 15:18:10 +0200

    new components

--- src/views/RealEstate.vue
+++ src/views/RealEstate.vue
@@ -1,12 +1,24 @@
 <template>
   <div class="real-estate">
-    RealEstate
+    <hr>
+    <city-selector></city-selector>
+    <hr>
+    <hood-selector></hood-selector>
+    <hr>
   </div>
+
 </template>
 
 <script>
+import CitySelector from '../components/RealEstate/CitySelector.vue';
+import HoodSelector from '../components/RealEstate/HoodSelector.vue';
+
 export default {
   name: 'RealEstate',
+  components: {
+    HoodSelector,
+    CitySelector,
+  },
 };
 </script>
 
