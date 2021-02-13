0f00338470f40e90f19ef00c811491b56a24b704
aleks <aleks.kinokus@gmail.com>
2021-02-13 09:23:51 +0200

    using lightbox

--- src/main.js
+++ src/main.js
@@ -3,10 +3,12 @@ import axios from 'axios';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faUserSecret } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import VueLazyLoad from 'vue-lazyload';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import vuetify from './plugins/vuetify';
+
 // import 'roboto-fontface/css/roboto/roboto-fontface.css';
 // import 'material-design-icons-iconfont/dist/material-design-icons.css';
 // import '@fortawesome/fontawesome-free/css/all.css';
@@ -14,7 +16,7 @@ import vuetify from './plugins/vuetify';
 
 library.add(faUserSecret);
 Vue.component('font-awesome-icon', FontAwesomeIcon);
-
+Vue.use(VueLazyLoad);
 Vue.config.productionTip = false;
 
 new Vue({
